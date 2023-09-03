fd0a82a0d08aa94c262911c0ae7c58af1eada43f
JimCle <142079260+JimCle@users.noreply.github.com>
2023-09-03 17:29:19 +0800

    my ninth code

---
+++ test9.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+
+struct stupid
+{
+	char name[23];
+	int age;
+	double height;
+	double weight;
+};
+
+struct stupid a = { "李婷",20,1.64,54.8 };
+
+int main()
+{   
+	printf("stupid成员资料\n");
+	printf("姓名:%s\n 年龄:%d\n 身高:（m）%lf\n 体重:（kg)%lf\n",a.name,a.age,a.height,a.weight);
+
+	return 0;
+}
