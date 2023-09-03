0ab2e23b820e5c178075665f1e1d4d248f3824e8
JimCle <142079260+JimCle@users.noreply.github.com>
2023-09-03 20:13:38 +0800

    my tenth code

---
+++ test10.c
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   1
+
+#include<stdio.h>
+
+int is_prime(int number)
+{
+	if (number <= 1)
+	{
+
+		return 0;
+	}
+	for (int a = 2;a * a <= number;a++) {
+		if (number % a == 0)
+		{
+			return 0;
+		}
+		
+	}
+	return 1;
+}
+
+int main()
+{
+	int number;
+
+	printf("请输入一个整数");
+	scanf("%d", &number);
+
+	if (is_prime(number))
+	{
+		printf("%d 是素数\n", number);
+
+	}
+	else
+	{
+		printf("%d 不是素数\n", number);
+
+	}
+	return 0;
+}
