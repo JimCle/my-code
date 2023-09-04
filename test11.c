0ad93119b518139123342e3a2ba9c0339d4daaee
JimCle <142079260+JimCle@users.noreply.github.com>
2023-09-04 15:08:14 +0800

    my eleventh code

---
+++ test11.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   1
+#include<stdio.h>
+
+int big(int a, int b)
+{
+	if (a > b)
+	{
+		return a;
+	}
+	else
+	{
+		return b;
+	}
+}
+
+int main()
+{
+	printf("请输入两个不相等的整数");
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+	int final = big(a, b);
+	printf("%d 是较大值", final);
+}
