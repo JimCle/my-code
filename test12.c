3b11fb23b225c17ec0b90f61f7ddae829eb8c942
JimCle <142079260+JimCle@users.noreply.github.com>
2023-09-04 16:20:49 +0800

    my twelfth code

---
+++ test12.c
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+
+//画了一架小飞机嘿嘿
+int main()
+{
+	printf("     **     \n");
+	printf("     **     \n");
+	printf("*************\n");
+	printf("*************\n");
+	printf("    *   *    \n");
+	printf("    *   *    \n");
+	
+	return 0;
+}
