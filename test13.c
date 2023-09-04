f84b7dc496ff3c762245fc255c212ba9b46f8ce4
JimCle <142079260+JimCle@users.noreply.github.com>
2023-09-04 16:41:20 +0800

    my thirteenth code

---
+++ test13.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   1
+
+#include<stdio.h>
+int muu(int M)
+{
+	if (M % 5 == 0)
+		printf("YES\n");
+	else
+		printf("NO\n");
+
+	return 0;
+}
+int main()
+{
+	printf("请输入一个>=1,<=1000000的整数\n");
+	int M;
+	scanf("%d", &M);
+	int result=muu(M);
+	printf("%s\n", result);
+	return 0;
+}
