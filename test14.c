c5762530f6c0781efd048da80f33b133dc995795
JimCle <142079260+JimCle@users.noreply.github.com>
2023-09-05 17:20:34 +0800

    my fourteenth code

---
+++ test14.c
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   1
+
+#include<stdio.h>
+int main()
+{
+	int x ;
+	scanf("%d", &x);
+	if (x % 2 == 0)//判断一个数是否为奇数
+		printf("不是奇数\n");
+	else
+		printf("是奇数\n");
+	return 0;
+}
