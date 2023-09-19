2eaefbcca9b483c7649a13cb8c29cd4360d9e8b8
JimCle <142079260+JimCle@users.noreply.github.com>
2023-09-19 20:47:48 +0800

    my seventeenth code

---
+++ test17.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   1
+
+#include<stdio.h>
+int main()
+{
+	int n = 10;
+	int m = 1;
+	int i = 1;
+	int b = 0;
+	for (m = 1;m <=n;m++)//计算1!+2!+3!+...+10！
+	{
+	    i*= m;		
+		b += i;
+
+	}
+	printf("%d", b);
+	return 0;
+}
