50a34f93cb35f9c9fc7a4f394bf91e63583c4614
JimCle <142079260+JimCle@users.noreply.github.com>
2023-09-24 10:04:34 +0800

    my eighteenth code

---
+++ test18.c
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   1
+
+#include<stdio.h>
+//在一个有序数组查找一个数，采用二分查找（折半查找）
+int main()
+{
+	int arr[] = { 2,3,4,5,6,7,8,9,10,11,12,13,14};
+	int l = 0;
+	int sz = sizeof(arr)/sizeof(arr[0]);
+	int r = sz-1;
+	int k = 15;
+	do
+	{
+		int m = (l + r) / 2;
+		if (arr[m] < k)
+		{
+			l = m + 1;
+		}
+		else if (arr[m] > k)
+		{
+			r = m - 1;
+		}
+		else
+		{
+			printf("找到了,下标是：%d\n", m);
+			break;
+		}
+	} 
+	while (r >= l);
+	if (r <l)
+	{
+		printf("找不到");
+	}
+	return 0;
+}
