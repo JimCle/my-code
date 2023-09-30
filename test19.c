d2e28e80916995a9a45b80ee9d4427ec656f6a5a
JimCle <142079260+JimCle@users.noreply.github.com>
2023-09-30 14:25:49 +0800

    my nineteenth code

---
+++ test19.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   1
+
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+//任务：张雅萌请原封不动地回来并穿裙子
+int main()
+{
+	char arr1[] = "张雅萌请原封不动地回来并穿裙子";
+	char arr2[] = "##############################";
+	int left = 0;
+	int right = strlen(arr1) - 1;
+	while(right>left)
+	{arr2[left] = arr1[left];
+	arr2[right] = arr1[right];
+	left++;
+	right--;
+
+	printf("%s\n", arr2);
+	Sleep(1);
+	system("cls");
+	}
+	printf("张雅萌请原封不动地回来并穿裙子");
+
+	return 0;
+}
+//int main()
+//{
+//	char arr1[] = "张雅萌请原封不动地回来并穿裙子";
+//	int str=strlen(arr1);
+//	printf("%d\n", str);
+//
+//	return 0;
+//}
