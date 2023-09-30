137f14f879dec2cd47726f785e6ff2c310819b94
JimCle <142079260+JimCle@users.noreply.github.com>
2023-09-30 16:39:42 +0800

    my twentieth code

---
+++ test20.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   1
+
+#include<stdio.h>
+#include<string.h>
+//模拟登录账号，输入密码，三次机会
+int main()
+{
+	int a=0;
+	char password[6] = { 0 };
+	
+	for (a = 0; a < 3; a++)
+	{
+		printf("请输入密码\n");
+		scanf("%s", password);
+		if (strcmp(password, "182076") == 0)
+		{
+			printf("成功登入\n");
+			break;
+		}
+		else
+			printf("密码错误，重新输入\n");
+			
+
+	}
+	if (a == 3)
+		printf("密码错误次数过多，将锁屏\n");
+
+	return 0;
+ }
